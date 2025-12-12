30e7e4e821686689c670b7fa3786981fcb7a44a7
AAYUR GANESH <aayurganesh2007@gmail.com>
2025-12-12 17:43:37 +0530

    Add files via upload

---
+++ database_connections.py
@@ -0,0 +1,19 @@
+def open_database_connection(db_name:str)->sql.Connection:
+    '''Opens a connection to the specified SQLite database and returns the connection object.
+    args:
+        db_name (str): The name of the database file to connect to.
+    returns:
+        sql.Connection: The SQLite database connection object.
+    '''
+    import sqlite3 as sql
+    conn = sql.connect(db_name)
+    return conn
+
+def close_database_connection(conn:sql.Connection)->None:
+    '''Closes the given SQLite database connection.
+    args:
+        conn (sql.Connection): The SQLite database connection object to close.
+    returns:
+        None
+    '''
+    conn.close()
