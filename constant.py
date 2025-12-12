b81f82647d183936c95686536061b2f6d8780cd6
AAYUR GANESH <aayurganesh2007@gmail.com>
2025-12-12 17:42:34 +0530

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,5 @@
+''' This module contains constant values used across the Expense Manager application, to prevent redundancy and ensure consistency. '''
+
+constants = {'valid_categories': ['food', 'transport', 'utilities', 'entertainment', 'miscellaneous'],
+    'valid_payment_methods': ['cash','upi','credit card', 'debit card', 'netbanking','wallet']
+}
